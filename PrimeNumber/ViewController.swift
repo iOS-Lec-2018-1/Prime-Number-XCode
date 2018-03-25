a406d9e892352c4a0abb7ec7d38e5e38fcfe5906
Jonghyun Kim <jhkim@dit.ac.kr>
2018-03-25 23:51:29 +0900

    add prime number check true & false

--- PrimeNumber/ViewController.swift
+++ PrimeNumber/ViewController.swift
@@ -9,17 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    var isPrime = true
+    var number: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func checkBtnPressed(_ sender: Any) {
+        number  = Int(myTextField.text!)!
+        print(number)
+        
+        if number == 1 {
+            isPrime = true
+        }
+        
+        if number != 1 && number != 2 {
+            for i in 2 ..< number {
+                if number % i == 0 {
+                    isPrime = false
+                }
+            }
+            //resultLabel.text = String(isPrime)
+        }
+        resultLabel.text = String(isPrime)
     }
-
-
+    
+    @IBAction func initBtnPressed(_ sender: Any) {
+        myTextField.text = ""
+        resultLabel.text = ""
+    }
+    
 }
 
